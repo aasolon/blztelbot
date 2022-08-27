4257bca89d4e9288924fa4f5594bc94711e6f121
Солонщиков Артём Александрович <solonshchikov.a.a@sberbank.ru>
2022-08-27 10:29:49 +0300

    Fix verbose log Fix telegram empty message

--- src/main/java/com/rtm/blztelbot/service/BlzTelBotService.java
+++ src/main/java/com/rtm/blztelbot/service/BlzTelBotService.java
@@ -2,6 +2,7 @@ package com.rtm.blztelbot.service;
 
 import com.google.common.base.Splitter;
 import com.rtm.blztelbot.telegrambot.BlzTelBot;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -27,7 +28,9 @@ public class BlzTelBotService {
     public void sendMessageToMe(String text) {
         long chatId = Long.parseLong(System.getenv("TELEGRAM_ADMIN_CHAT_ID"));
         for(final String token : Splitter.fixedLength(4000).split(text)) {
-            blzTelBot.sendMessage(chatId, token);
+            if (StringUtils.isNotEmpty(token)) {
+                blzTelBot.sendMessage(chatId, token);
+            }
         }
 
     }
