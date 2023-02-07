c13fce336faa5c5f7cfaa0a7962449061ff13e37
Солонщиков Артём Александрович <solonshchikov.a.a@sberbank.ru>
2023-02-08 00:14:53 +0300

    CIV6. Get telegram name by civ6 name from DB.

--- src/main/java/com/rtm/blztelbot/service/Civ6Service.java
+++ src/main/java/com/rtm/blztelbot/service/Civ6Service.java
@@ -1,15 +1,19 @@
 package com.rtm.blztelbot.service;
 
+import com.rtm.blztelbot.entity.Civ6Player;
 import com.rtm.blztelbot.model.Civ6Webhook;
+import com.rtm.blztelbot.repository.Civ6PlayerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Civ6Service {
 
     private final BlzTelBotService blzTelBotService;
+    private final Civ6PlayerRepository civ6PlayerRepository;
 
-    public Civ6Service(BlzTelBotService blzTelBotService) {
+    public Civ6Service(BlzTelBotService blzTelBotService, Civ6PlayerRepository civ6PlayerRepository) {
         this.blzTelBotService = blzTelBotService;
+        this.civ6PlayerRepository = civ6PlayerRepository;
     }
 
     public void processCiv6Webhook(Civ6Webhook webhook) {
@@ -17,6 +21,9 @@ public class Civ6Service {
         String playerName = webhook.getValue2();
         String turnNumber = webhook.getValue3();
 
-        blzTelBotService.sendMessageToChatId(-601860434, "Эй, " + playerName + ", ходи давай!");
+        Civ6Player civ6Player = civ6PlayerRepository.findByCivName(playerName);
+
+        String message = String.format("Эй, @%s, ходи давай! (игра: %s, ход: %s)", civ6Player.getTelegramName(), gameName, turnNumber);
+        blzTelBotService.sendMessageToChatId(-601860434, message);
     }
 }
