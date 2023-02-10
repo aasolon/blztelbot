1aff90408d0c421f5b4ba86d46d7d2638d43669c
Солонщиков Артём Александрович <solonshchikov.a.a@sberbank.ru>
2023-02-11 00:51:42 +0300

    Civ6 notify with telegram name or player name from webhook

--- src/main/java/com/rtm/blztelbot/service/Civ6Service.java
+++ src/main/java/com/rtm/blztelbot/service/Civ6Service.java
@@ -3,6 +3,7 @@ package com.rtm.blztelbot.service;
 import com.rtm.blztelbot.entity.Civ6Player;
 import com.rtm.blztelbot.model.Civ6Webhook;
 import com.rtm.blztelbot.repository.Civ6PlayerRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,12 +19,13 @@ public class Civ6Service {
 
     public void processCiv6Webhook(Civ6Webhook webhook) {
         String gameName = webhook.getValue1();
-        String playerName = webhook.getValue2();
+        String webhookPlayerName = webhook.getValue2();
         String turnNumber = webhook.getValue3();
 
-        Civ6Player civ6Player = civ6PlayerRepository.findByCivName(playerName);
+        Civ6Player civ6Player = civ6PlayerRepository.findByCivName(webhookPlayerName);
+        String playerName = StringUtils.defaultString(civ6Player.getTelegramName(), webhookPlayerName);
 
-        String message = String.format("Эй, @%s, ходи давай! (игра: %s, ход: %s)", civ6Player.getTelegramName(), gameName, turnNumber);
+        String message = String.format("Эй, @%s, ходи давай! (игра: %s, ход: %s)", playerName, gameName, turnNumber);
         blzTelBotService.sendMessageToChatId(-601860434, message);
     }
 }
